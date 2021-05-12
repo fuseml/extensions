408c5deddb6ba82dd68780f0ca08f313f40d3c56
Jiří Suchomel <jiri.suchomel@suse.com>
2021-05-12 17:32:40 +0200

    Let the inference service register application in fuseml
    Related to https://github.com/fuseml/fuseml/issues/75

--- inference-services/kfserving/run.sh
+++ inference-services/kfserving/run.sh
@@ -29,3 +29,31 @@ isvc="${ORG}-${PROJECT}"
 kubectl wait --for=condition=Ready --timeout=600s inferenceservice/${isvc}
 prediction_url="$(kubectl get inferenceservice/${isvc} -o jsonpath='{.status.url}')/v2/models/${isvc}/infer"
 printf "${prediction_url}" > /tekton/results/${TASK_RESULT}
+
+# Now, register the new application within fuseml
+
+inside_metadata=""
+first_resource=1
+envsubst < /root/template.sh | while read line
+do
+  if echo $line | grep -q '^[ ]*kind'; then
+      inside_metadata=""
+      if [[ $first_resource == 0 ]] ; then
+          echo -n ", " >> /tmp/resources.json
+      else
+          first_resource=0
+      fi
+      echo -n "{$line" | sed -E 's/([^ \t:{]+)/"\1"/g' >> /tmp/resources.json
+  fi
+  if echo $line | grep -q '^[ ]*metadata:' ; then
+      inside_metadata="yes"
+  fi
+  if echo $line | grep -q '^[ ]*name:' && [[ -n $inside_metadata ]] ; then
+      echo -n ", $line}" | sed -E 's/([^ \t:,}]+)/"\1"/g' >> /tmp/resources.json
+  fi
+done
+
+resources=$(cat /tmp/resources.json | tr -s '"')
+rm /tmp/resources.json
+
+curl -X POST -H "Content-Type: application/json"  http://fuseml-core:80/applications -d "{\"name\":\"$isvc\",\"type\":\"predictor\",\"url\":\"$prediction_url\",\"workflow\":\"$FUSEML_ENV_WORKFLOW_NAME\", \"k8s_namespace\": \"$FUSEML_ENV_WORKFLOW_NAMESPACE\", \"k8s_resources\": [ $resources ]}"
